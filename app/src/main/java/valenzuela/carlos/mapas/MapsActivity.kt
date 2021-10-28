1888bb719dc25bf69bbb9ce72c50eec5c24933f9
Carlos Valenzuela <luzbelshin@gmail.com>
2021-10-27 23:15:33 -0700

    First commit

---
+++ app/src/main/java/valenzuela/carlos/mapas/MapsActivity.kt
@@ -0,0 +1,51 @@
+package valenzuela.carlos.mapas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import valenzuela.carlos.mapas.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val itsonN = LatLng(27.4941872,-109.9728435)
+        val itsonC = LatLng(27.4830612,-109.9331572)
+        mMap.addMarker(MarkerOptions().position(itsonN).title("Marker in ITSON, Nainari"))
+        mMap.addMarker(MarkerOptions().position(itsonC).title("Marker in ITSON, Centro"))
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(itsonN, 10.0f))
+    }
+}
